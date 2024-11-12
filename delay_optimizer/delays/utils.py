5f4343648d3ada8d67a5ce7e49600c7f0233faac
cayjobla <cayjobla@gmail.com>
2024-11-12 14:18:28 -0700

    WIP update to parameter buffer

--- delay_optimizer/delays/utils.py
+++ delay_optimizer/delays/utils.py
@@ -8,9 +8,9 @@ class ParamHistoryBuffer:
         buffer_size,
         device = None,
     ):
-        self.buffer_size = buffer_size
-        self.device = device or param.device
-        self._initialize_history(param)
+        self.buffer_size = buffer_size      # Should be max_L+1
+        self._buffer = param.repeat(self.buffer_size, *(1,)*param.ndim).detach()
+        self._current_idx = 0
 
     def __getattr__(self, name):
         return getattr(self._buffer, name)
@@ -19,14 +19,9 @@ class ParamHistoryBuffer:
         return ("ParamHistoryBuffer(\n"
                 f"{self._buffer}, current_idx={self._current_idx})")
 
-    def _initialize_history(self, param):
-        """Initialize the history with L copies of the current parameter."""
-        self._buffer = param.repeat(self.buffer_size, *(1,)*param.ndim).detach()
-        self._current_idx = 0
-        
     def _delay_to_idx(self, delay):
-        if any(delay <= 0) or any(delay > self.buffer_size):
-            raise IndexError(f"Delay must be in [1, {self.buffer_size}]")
+        if any(delay < 0) or any(delay > self.buffer_size):
+            raise IndexError(f"Delay must be in [0, {self.buffer_size}]")
         return (self._current_idx + delay - 1) % self.buffer_size
 
     def __getitem__(self, args):
